629a741507673b24956b38451ea573057bb525ca
ramsharan <ramsharan@ramsharan-Inspiron-N5110>
2013-02-17 19:09:56 +0545

    little bit more

--- myDownloadManager/src/core/Downloader.java
+++ myDownloadManager/src/core/Downloader.java
@@ -22,6 +22,10 @@ public class Downloader {
     protected int size;
     protected int downloaded;
     
+    //for partition download
+    protected int totalSize;
+    protected int Psize[];
+    protected int Pdownloaded[];
     
     public Downloader(URL url){
         this.url = url;
@@ -29,6 +33,12 @@ public class Downloader {
         this.size = -1;
         
         download();
+    }
+    public Downloader(URL url,int dummy){
+        this.url = url;
+        Psize = new int[2];
+        Pdownloaded = new int[2];
+        Pdownload();
     }
 
     
@@ -38,19 +48,20 @@ public class Downloader {
             connection.setRequestProperty("Range", "bytes="+downloaded+"-");
             connection.connect();
             System.out.println(""+connection.getResponseCode());
-            System.out.println(""+connection.getContentLength()+"B");
-            RandomAccessFile file = new RandomAccessFile("abcabc.pdf", "rw");
+            System.out.println(""+(size=connection.getContentLength())/(1024.0*1024)+"MB");
+            RandomAccessFile file = new RandomAccessFile("abcabc.wma", "rw");
             file.seek(downloaded);
             InputStream stream = connection.getInputStream();
             while(true){
-                byte b[] = new byte[1024];
+                byte b[] = new byte[1024*40];
                 int read = stream.read(b);
                 if (read ==-1) break;
                 downloaded+=read;
-                System.out.println(""+downloaded);
+                System.out.println("downloaded = "+downloaded/(1024.0) +"KB");
+                System.out.println("remaining = "+(size - downloaded)/(1024.0) + "KB");
                 file.write(b, 0, read);
 //                try {
-//                    Thread.sleep(10);
+//                    Thread.sleep(1000);
 //                } catch (InterruptedException ex) {
 //                    Logger.getLogger(Downloader.class.getName()).log(Level.SEVERE, null, ex);
 //                }
@@ -60,5 +71,54 @@ public class Downloader {
         } catch (IOException ex) {
             Logger.getLogger(Downloader.class.getName()).log(Level.SEVERE, null, ex);
         } 
+    }
+
+    private void Pdownload() {
+        HttpURLConnection connection[] = new HttpURLConnection[2];
+        InputStream stream[] = new InputStream[2];
+        try {
+            connection[0] = (HttpURLConnection) url.openConnection();
+            connection[0].setRequestProperty("Range", "bytes="+Pdownloaded[0]+"-");
+            connection[0].connect();
+            System.out.println(""+Pdownloaded[0]);
+            totalSize = connection[0].getContentLength();
+            System.out.println("total size = "+(totalSize/1024.0)+"KB");
+            Psize[0] = totalSize/2;
+            Psize[1] = totalSize - Psize[0];
+            connection[1] = (HttpURLConnection) url.openConnection();
+            connection[1].setRequestProperty("Range", "bytes="+(Psize[0])+"-");
+            connection[1].connect();
+            
+            stream[0] = connection[0].getInputStream();
+            stream[1] = connection[1].getInputStream();
+            
+            RandomAccessFile file0 = new RandomAccessFile("jpt.wma", "rw");
+            RandomAccessFile file1 = new RandomAccessFile("jpt.wma", "rw");
+            file0.seek(Pdownloaded[0]);
+            file1.seek(Psize[0]);
+            
+            while(true){
+                byte b0[] = new byte[1024*4];
+                byte b1[] = new byte[1024*4];
+                int read0 = stream[0].read(b0);
+                int read1 = stream[1].read(b1);
+                if (read0 ==-1 && read1==-1) break;
+                if(read0!=-1){
+                    Pdownloaded[0]+=read0;
+                    file0.write(b0, 0, read0);
+                }
+                if(read1!=-1){
+                    Pdownloaded[1]+=read1;
+                    file1.write(b1, 0, read1);
+                }
+                downloaded = Pdownloaded[0]+Pdownloaded[1];
+                if(downloaded>=totalSize+1) break;
+                System.out.println("downloaded = "+(downloaded/1024.0)+"KB");
+                System.out.println("remaining = "+((totalSize - downloaded)/1024.0)+"KB");
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Downloader.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- myDownloadManager/src/test/Main.java
+++ myDownloadManager/src/test/Main.java
@@ -7,9 +7,6 @@ package test;
 import core.Downloader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.Time;
-import java.util.Date;
-import java.util.Timer;
 
 /**
  *
@@ -17,7 +14,9 @@ import java.util.Timer;
  */
 public class Main {
     public static void main(String string[]) throws MalformedURLException{
-        URL url = new URL("http://localhost/test/LFS-BOOK-7.2.pdf");
+        URL url = new URL("http://localhost/test/Untitled.wma");
+        //URL url = new URL("http://d19vezwu8eufl6.cloudfront.net/images/recoded_videos%2F06_05%20-%20Video%20Level%20Sets%20and%20Curve%20Evolution%20%5Bf3f5e6e2%5D%20.mp4");
+        //URL url = new URL("http://photography.nationalgeographic.com/staticfiles/NGS/Shared/StaticFiles/Photography/Images/Content/mountain-hut-584962-sw.jpg");
         Downloader d = new Downloader(url);
         
         //System.out.println(""+url.getFile());
